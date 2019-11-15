de14eb9201b80426ca3cfc35cd73a7b15e1c645d
Shanda Woods <shandawoods@Shandas-Mini.lan1>
2019-11-15 13:49:04 -0500

    MVP complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, CardTitle, CardText, CardColumns, Collapse, Button, } from 'reactstrap';
+import '../index.css';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharacterCard = props => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen (!isOpen);
+
+  return (
+    <div className="person-data" key={props.id}>
+      <Button color="info" onClick={toggle} style={{ marginBottom: '1rem'}}>Click to see if I'm Pickle Rick</Button>
+        <Collapse isOpen={isOpen}>
+          <CardColumns>
+            <Card body inverse color="success">
+      <div className="cardInfo">
+        <CardTitle>Name:{props.name}</CardTitle>
+        <CardText>Species:{props.species}</CardText>
+        <CardText>Type:{props.type}</CardText>
+      </div>
+            </Card>
+          </CardColumns>
+        </Collapse>    
+    </div>
+  ); 
+  
 }
+
+export default CharacterCard;
